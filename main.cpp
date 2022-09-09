6145c2edf43c818c76519db1fd50342758ec22d6
nemethmiklos1996 <nemeth.miklos.1996@gmail.com>
2022-09-09 12:19:27 +0200

    MOD: feltárt hibák javítása, felesleges kommentek eltávolítása

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int* b = new int[NELEMENTS];                            // alulvonás hiányzik az N_ELEMENTS-bõl
-    std::cout << '1-100 ertekek duplazasa'                  // sorvégi ; hiányzik, nem megfelelõ a szöveg jelölése (' helyett " szükséges), hiányzik a sortörés
-        for (int i = 0;)                                    // ciklus hiányos (a meddig érték és a léptetés mértéke/iránya hiányzik), rosszul van tabulálva 
-        {
-            b[i] = i * 2;                                   // i + 1. értéket kell 2-vel szorozni, mert 1-tõl 100-ig kell a számokat 
-        }
-    for (int i = 0; i; i++)                                 // hiányzik a második i-re vonatkozó feltétel (meddig mehet a ciklus) 
+    int* b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"                               // sorvégi ; hiányzik, nincs megadva az érték, nincs sortörés
+        b[i] = (i + 1) * 2;
+    }
+    for (int i = 0; i < N_ELEMENTS; i++)
+    {
+        std::cout << "Ertek: " << b[i] << std::endl;
     }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;                                              // nincs kezdõ értéke a változónak
-    for (int i = 0; i < N_ELEMENTS, i++)                    // , helyett ; kell használni a ciklusban, 
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]                                       // sorvégi ; hiányzik
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
